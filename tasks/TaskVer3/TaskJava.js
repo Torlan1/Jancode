fbf464afc76c39bd12bd1ba6b1d8cef45df8c3f3
Jan M <gauge90@gmail.com>
2019-10-28 13:13:31 -0400

    Signed-off-by: Jan M <gauge90@gmail.com> updated buttons/table fucntion

--- tasks/TaskVer3/TaskJava.js
+++ tasks/TaskVer3/TaskJava.js
@@ -1,16 +1,18 @@
 var b = "OpenItems"
+
+/* This is for the Buttons/Table to make active on click*/
 function BtnFuction(Sender) {
-  document.getElementById(Sender).className = "ButtonActive";
-  document.getElementById(b).className = "BtnNonActive";
-  b=Sender;
-
-
- 
-    var x = document.getElementById("SelectTable");{
-        if (x.style.visibility === "hidden") {
-          x.style.visibility = "visible";
-        } else {
-          x.style.visibility = "hidden";
-        }
-    }
-  } 
+  
+  if (b != Sender) {
+    /*This handles the buttons*/
+      document.getElementById(Sender).className = "ButtonActive";
+      document.getElementById(b).className = "BtnNonActive";
+    
+    /*This handles the tables*/
+      document.getElementById(Sender + "Table").className = "SelectedTable";
+      document.getElementById(b + "Table").className = "HiddenTable";
+      
+    /*This remebers the new active table*/
+      b=Sender;}
+  
+  };
